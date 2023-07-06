57f0e50dc051f7ae63ad56d4aea545060561da35
Vaibhavpendke <vaibhavpendke56@gmail.com>
2023-07-07 00:09:25 +0530

    initial commit

---
+++ app/src/main/java/com/example/helthcare/user_Info.java
@@ -0,0 +1,69 @@
+package com.example.helthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class user_Info extends AppCompatActivity {
+    ImageView app,test,medi,diet1;
+    TextView maps;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+
+        app=findViewById(R.id.app);
+        test=findViewById(R.id.test);
+        medi=findViewById(R.id.medi);
+        diet1=findViewById(R.id.diet1);
+        maps=findViewById(R.id.textViewNewUser);
+
+        app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(user_Info.this,Appointment.class);
+                startActivity(intent);
+            }
+        });
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(user_Info.this,Testpage.class);
+                startActivity(intent);
+            }
+        });
+
+        medi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(user_Info.this,prescription.class);
+                startActivity(intent);
+            }
+        });
+
+        diet1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(user_Info.this,dietplan.class);
+                startActivity(intent);
+            }
+        });
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(user_Info.this,MapsActivity.class));
+            }
+        });
+
+
+    }
+}
